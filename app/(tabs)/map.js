261ab746fadd1e8e78b0e38b9fea53c186d8cdfa
Jeanne Michel <jeanne.michel@he-arc.ch>
2023-06-21 16:02:10 +0200

    map setup

--- app/(tabs)/map.js
+++ app/(tabs)/map.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+import { useState } from 'react';
+import { UrlTile } from 'react-native-maps';
+import { Dimensions } from 'react-native';
 
 const Map = () => {
+
+    const [region, setRegion] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+
     return (
-        <View>
-            <Text>Map</Text>
-        </View>
+
+            <MapView
+                //minHeight={height}
+                style={{ flex: 1 }}
+                region={region}
+                //onRegionChange={region => setRegion(region)}
+            >
+                <UrlTile
+                    urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    maximumZ={19}
+                    flipY={false}
+                />
+            </MapView>
     );
 };
 
